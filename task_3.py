c100528092017ea28e53d3c8f8f918cb3a18f199
Raccoo_N_Roll <Raccoo-n-Roll@yandex.kz>
2026-07-22 05:02:46 +0500

    task_3: исправление после ревью, печатаем словарь за один проход

--- task_3.py
+++ task_3.py
@@ -7,13 +7,13 @@ world_champions = {
 }
 
 world_champions[2022] = 'Аргентина'
-# Печатаем содержимое словаря + новый элемент
-for year in world_champions.keys():
-    print(year,'-', world_champions[year])
+# Печатаем содержимое словаря + новый элемент, получаем пару сразу
+for year, country_name in world_champions.items():
+    print(year, '-', country_name)
 
+# исправлено условие проверки
 country = 'Италия'
 if country in world_champions:
     print('Италия cтановилась чемпионом мира по футболу в 21 веке!')
 else:
     print('Италия не выигрывала чемпионат мира по футболу в 21 веке.')
-
